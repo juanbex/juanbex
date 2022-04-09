cd4f2fed559b7268070b3e28ac3cc5f2ff3e16f9
Juan Bueno <juanbexnice@gmail.com>
2022-04-08 23:40:25 -0500

    Se agrega debugging function

---
+++ debugging.py
@@ -0,0 +1,27 @@
+def divisors(num):
+    divisors = []
+    for i in range (1, num + 1):
+        if num % i == 0:
+            divisors.append(i)
+    return divisors
+
+def palindrome(string):
+    return string == string[::-1]
+
+def fibonacci():
+    # Fibonacci series:
+    # the sum of two elements defines the next
+    a, b = 0, 1
+    while a < 10:
+        print(a)
+        a, b = b, a + b
+
+def run():
+    num = int(input("Ingrese un numero: "))
+    print(divisors(num))
+    print("Terminó el programa.")
+
+    print(palindrome(1))
+
+if __name__ == '__main__':
+    run()
